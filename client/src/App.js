e4cba97d42b235375edcedecbf07b4904d919630
arunachalam <arunachaleshwaran12168@gmain.com>
2022-02-01 11:42:54 +0530

    added the sending file to server logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
 
 function App() {
+  const onChange = async e => {
+    const data = new FormData();
+    data.append('userPhoto', e.target.files[0]);
+    const response = await fetch('http://localhost:5000/uploadimage/',
+      {
+        method: 'POST',
+        body: data
+      })
+    console.log(response)
+  }
   return (
     <div className="App">
+      <input type="file" name="userPhoto" onChange={onChange} id="userPhoto" accept=".png,.jpg,.jpeg" />
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import Express from "express";
 import cors from "cors";
 import recordRoutes from "./routes/record.js";
 import dbObject from "./db/conn.js";
+import bodyParser from 'body-parser';
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -11,10 +12,35 @@ const app = Express();
 app.use(cors());
 app.use(Express.json());
 app.use(recordRoutes);
-// get driver connection
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
  
 app.listen(port, () => {
   // perform a database connection when server starts
   dbObject.connectToServer((err) => { if (err) console.error(err) });
   console.log(`Server is running on port: ${port}`);
 });
+
+
+import multer from "multer";
+const storage = multer.diskStorage({
+	destination: function (req, file, callback) {
+		callback(null, './uploads');
+	},
+	filename: function (req, file, callback) {
+		callback(null, Date.now()+ '-' +file.originalname);
+	}
+});
+const upload = multer({ storage }).single('userPhoto');
+
+app.post('/uploadimage', function (req, res) {
+	upload(req, res, function (err) {
+		if (err) {
+			return res.end("Error uploading file.");
+		}
+		res.end("File is uploaded");
+	});
+});
+// app.get('/image',(req,res)=>console.log('hi'))
+// app.use(Express.static('./uploads'));
+app.use('/getimage',Express.static('./uploads'))
